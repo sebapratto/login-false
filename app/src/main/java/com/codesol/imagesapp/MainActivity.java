6b7eb31d89bcbe38427d1503c1e51c0337e248a7
Sebastian Pratto <pratto.sebastian.ignacio@gmail.com>
2021-09-14 00:43:10 -0300

    commit two

--- app/src/main/java/com/codesol/imagesapp/MainActivity.java
+++ app/src/main/java/com/codesol/imagesapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.codesol.imagesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String usu = "";
+    private String pwd = "";
+
+    private EditText et1;
+    private EditText et2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.etNombre);
+        et2 = (EditText) findViewById(R.id.etPassword);
+    }
+
+    public void Login(View view){
+        usu = String.valueOf(et1.getText().toString());
+        pwd = String.valueOf(et2.getText().toString());
+
+        if(usu.isEmpty()){
+            Toast.makeText(this, "El Usuario no puede estar vacio...", Toast.LENGTH_SHORT).show();
+        }
+        if(pwd.isEmpty()){
+            Toast.makeText(this, "El Password no puede estar vacio", Toast.LENGTH_SHORT).show();
+        }
+
+        if(usu.equals("admin") && pwd.equals("1234")){
+            Toast.makeText(this, "Felicidades, ha ingresado a la cuenta..!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Usuario y/o Password incorrecto...", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
